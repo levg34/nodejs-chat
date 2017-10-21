214dfb88c28da604a5d229876c2893b1d8018521
levg34 <levg34@users.noreply.github.com>
2017-10-21 14:30:39 +0200

    env variable private key problem

--- ttm.js
+++ ttm.js
@@ -14,7 +14,7 @@ admin.initializeApp({
 	credential: admin.credential.cert({
 		projectId: process.env.PROJECT_ID,
 		clientEmail: process.env.CLIENT_EMAIL,
-		privateKey: process.env.PRIVATE_KEY
+		privateKey: JSON.parse(process.env.PRIVATE_KEY)
 	}),
 	databaseURL: 'https://ttm-db.firebaseio.com'
 })
